fca3e34fd008b67dea2d51f3f1a95b65bc90331f
raffarj10 <raffarj10@gmail.com>
2023-10-20 23:33:02 +0400

    homeWork

--- rafael/21-10-23.js
+++ rafael/21-10-23.js
@@ -50,3 +50,62 @@ let str1 = 'Hello'
 let substr1 = 'ell'
 console.log(includes(str1,substr1))
 
+
+
+// to upperCase
+
+function upperCase(string){
+    let words = string.split(' ')
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+}
+
+console.log(upperCase('i love javascript'))
+
+
+
+// find longest word
+
+function longestWord(string){
+    let words = string.split(' ')
+    let longest = words[0]
+    words.forEach(word => {
+        if(word.length > longest.length){
+            longest = word
+        }
+    })
+    return longest
+}
+
+console.log(longestWord('Web Development Tutoria'))
+
+
+// return sum pf 2 numbers
+
+function sum(num1, num2){
+    return num1 + num2
+}
+
+console.log(sum(1,2))
+
+// calculator
+
+function calc(num1, num2, operator){
+    switch(operator){
+        case '+':
+            return num1 + num2
+        break;
+        case '-':
+            return num1 - num2
+        break;
+        case '*':
+            return num1 * num2
+        break;
+        case '/':
+            return num1 / num2
+        break;
+    }
+}
+
+console.log(calc(1,2,'+'))
+
+
