0706f51eb574f203ec883488bd6de07c97e0c032
Elmira <eavagyan@acutech.dev>
2023-11-29 19:10:53 +0400

    timer content

--- elmira/test/script.js
+++ elmira/test/script.js
@@ -1,40 +1,33 @@
-// let button = document.getElementById('btn');
-// let darkH1 = document.getElementById('dark');
-// console.log('Log ::: H1 ===', darkH1);
-// // JS version
-// // let clicked = false;
-// // button.addEventListener('click', () => {
-// //     if(clicked) {
-// //         darkH1.innerText = 'Sad div 3';
-// //     } else {
-// //         darkH1.innerText = 'Happy div 3';
-// //     }
-// //     clicked = !clicked;
-// // })
-//
-//
-// // button.addEventListener('click', () => {
-// //     darkH1.classList.toggle('happy');
-// // })
-//
-// //Potato task
-// const forbiddenKey = 'potato';
-// const textElement = document.getElementById('text');
-// button.addEventListener('click', () => {
-//     let innerText = textElement.innerText;
-//     console.log('Log ::: textElement ===', innerText);
-//     innerText = innerText.replace(forbiddenKey, 'cute');
-//     textElement.innerText = innerText;
-//     console.log('Log ::: innerText ===', innerText)
-// })
+document.addEventListener('DOMContentLoaded', function() {
+    const floors = document.querySelectorAll('.floor');
+    const lift = document.getElementById('lift');
 
-// Place Task
-const colors = ['greenyellow', '#ffda21', 'red'];
-let styleButton = document.getElementById('change-styles');
-let spanList =document.getElementsByClassName('place-span');
-console.log('Log ::: spanList ===', spanList);
-styleButton.addEventListener('click', () => {
-    let array = [...spanList];
-    array.map((item, index) => item.style.color = colors[index%3])
+    floors.forEach(floor => {
+        floor.addEventListener('click', function() {
+            const selectedFloor = this.dataset.floor;
+            moveLiftToFloor(selectedFloor);
+        });
+    });
 
-})
+    window.moveLift = function(direction) {
+        const currentFloor = parseInt(lift.style.bottom) / 300 + 1;
+
+        if (direction === 'up' && currentFloor < floors.length) {
+            moveLiftToFloor(currentFloor + 1);
+        } else if (direction === 'down' && currentFloor > 1) {
+            moveLiftToFloor(currentFloor - 1);
+        }
+    };
+
+    function moveLiftToFloor(targetFloor) {
+        const currentFloor = parseInt(lift.style.bottom) / 300 + 1;
+        const floorDifference = Math.abs(targetFloor - currentFloor);
+
+        lift.style.bottom = `${(targetFloor - 1) * 300}px`;
+
+        // Simulate lift movement delay
+        setTimeout(() => {
+            console.log(`Lift moved to Floor ${targetFloor}`);
+        }, 500 * floorDifference);
+    }
+});
